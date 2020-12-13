e62a5566fa127a628186fe17cb28c6982827fe70
Robert <1robertchambers@gmail.com>
2020-12-13 20:11:02 +0000

    add existing, update requirements for control panel

--- functional_requirements.mm
+++ functional_requirements.mm
@@ -1,4 +1,31 @@
 <map version="1.1.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1607888125977" ID="ID_1111700057" MODIFIED="1607888576138" TEXT="Functional Requirements"/>
+<node CREATED="1607888125977" ID="ID_1111700057" MODIFIED="1607888576138" TEXT="Functional Requirements">
+<node CREATED="1607889805944" ID="ID_333862331" MODIFIED="1607889817745" POSITION="right" TEXT="1. client side">
+<node CREATED="1607889819446" ID="ID_1408814252" MODIFIED="1607890190462" TEXT="1.1 control panel">
+<node CREATED="1607889824804" ID="ID_1144532847" MODIFIED="1607889831174" TEXT="1.1.1 bills button">
+<node CREATED="1607889831178" ID="ID_1389117676" MODIFIED="1607889991228" TEXT="1.1.1.1 bills button shall occpuy left 1.3 of control panel"/>
+<node CREATED="1607889993381" ID="ID_1483981848" MODIFIED="1607890035938" TEXT="1.1.1.2 the graphic shall be a drawing of a document"/>
+<node CREATED="1607889862325" ID="ID_815755414" MODIFIED="1607890107124" TEXT="1.1.1.3 when clicked the bills button shall show the bill search page">
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node CREATED="1607889884069" ID="ID_324079009" MODIFIED="1607889892961" TEXT="1.1.2 mp search button">
+<node CREATED="1607889896757" ID="ID_1555027702" MODIFIED="1607889975686" TEXT="1.1.2.1 mp search button shall occupy centre 1/3 of control panel"/>
+<node CREATED="1607890005637" ID="ID_1676445593" MODIFIED="1607890051890" TEXT="1.1.2.2 the graphic shall be a drawing of a persons head and shoulders"/>
+<node CREATED="1607890053013" ID="ID_1578113406" MODIFIED="1607890111524" TEXT="1.1.2.3 when clicked, the mp search page shall be displayed">
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node CREATED="1607890117910" ID="ID_1938244959" MODIFIED="1607890124599" TEXT="1.1.3 preferences button">
+<node CREATED="1607890124602" ID="ID_1005049821" MODIFIED="1607890141490" TEXT="1.1.3.1 preferences button shall occupy right 1/3 of control panel"/>
+<node CREATED="1607890141829" ID="ID_385737412" MODIFIED="1607890157922" TEXT="1.1.3.2 graphic shall be a cogwheel"/>
+<node CREATED="1607890159222" ID="ID_1264342571" MODIFIED="1607890193773" TEXT="1.1.3.3 when clicked, the preferences page shall be shown">
+<icon BUILTIN="password"/>
+</node>
+</node>
+</node>
+<node CREATED="1607890186982" ID="ID_1097690422" MODIFIED="1607890186982" TEXT=""/>
+</node>
+</node>
 </map>
