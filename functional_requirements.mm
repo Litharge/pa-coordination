b16cbfd9e7398244b1d43e4f05ad9d02830534e0
Robert <1robertchambers@gmail.com>
2020-12-13 21:25:16 +0000

    add existing, update requirements for mp search page

--- functional_requirements.mm
+++ functional_requirements.mm
@@ -4,7 +4,7 @@
 <node CREATED="1607889805944" ID="ID_333862331" MODIFIED="1607889817745" POSITION="right" TEXT="1. client side">
 <node CREATED="1607889819446" ID="ID_1408814252" MODIFIED="1607890860048" TEXT="1.1 control panel">
 <node CREATED="1607889824804" ID="ID_1144532847" MODIFIED="1607889831174" TEXT="1.1.1 bills button">
-<node CREATED="1607889831178" ID="ID_1389117676" MODIFIED="1607889991228" TEXT="1.1.1.1 bills button shall occpuy left 1.3 of control panel"/>
+<node CREATED="1607889831178" ID="ID_1389117676" MODIFIED="1607891497174" TEXT="1.1.1.1 bills button shall occupy left 1.3 of control panel"/>
 <node CREATED="1607889993381" ID="ID_1483981848" MODIFIED="1607890035938" TEXT="1.1.1.2 the graphic shall be a drawing of a document"/>
 <node CREATED="1607889862325" ID="ID_815755414" MODIFIED="1607890107124" TEXT="1.1.1.3 when clicked the bills button shall show the bill search page">
 <icon BUILTIN="password"/>
@@ -31,7 +31,9 @@
 <node CREATED="1607890507802" ID="ID_582132696" MODIFIED="1607890529539" TEXT="1.2.1.1.1 bill name at top of card"/>
 <node CREATED="1607890532582" ID="ID_33597705" MODIFIED="1607890550595" TEXT="1.2.1.1.2 short description shall appear below bill name"/>
 <node CREATED="1607890551078" ID="ID_791436401" MODIFIED="1607890567379" TEXT="1.2.1.1.3 favourite button shall appear below short description"/>
-<node CREATED="1607890895511" ID="ID_1542783817" MODIFIED="1607890906276" TEXT="1.2.1.1.3 when clicked, the corresponding bill details page will be displayed (see "/>
+<node CREATED="1607891984187" ID="ID_189480444" MODIFIED="1607892006071" TEXT="1.2.1.1.3 when clicked, the corresponding bill details page (1.2.2) will be displayed">
+<icon BUILTIN="password"/>
+</node>
 </node>
 <node CREATED="1607890597639" ID="ID_1175379026" MODIFIED="1607890650435" TEXT="1.2.1.2 card organisation">
 <node CREATED="1607890670055" ID="ID_560372424" MODIFIED="1607890676707" TEXT="1.2.1.2.1 each section">
@@ -55,6 +57,24 @@
 <node CREATED="1607891239834" ID="ID_1961008119" MODIFIED="1607891287861" TEXT="1.2.2.6.1 two side by side boxes, left box titled &quot;noes&quot;, right box titled &quot;ayes&quot;"/>
 <node CREATED="1607891291672" ID="ID_1744950072" MODIFIED="1607891309061" TEXT="1.2.2.6.2 left box shall contain names of mps who voted no"/>
 <node CREATED="1607891309688" ID="ID_369266253" MODIFIED="1607891321734" TEXT="1.2.2.6.3 right box shall contain names of mps who voted aye"/>
+</node>
+</node>
+</node>
+<node CREATED="1607891505305" ID="ID_316731000" MODIFIED="1607891509557" TEXT="1.3 mp search page">
+<node CREATED="1607891553369" ID="ID_1887862229" MODIFIED="1607891560934" TEXT="1.3.1 local mp info box"/>
+<node CREATED="1607891561227" ID="ID_1714283843" MODIFIED="1607892245754" TEXT="1.3.2 search boxes shall appear below local mp info box">
+<icon BUILTIN="xmag"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1607891572107" ID="ID_1151529740" MODIFIED="1607892238312" TEXT="1.3.2.1 &quot;search by&quot; dialogue box shall appear on the left"/>
+</node>
+<node CREATED="1607892047869" ID="ID_706271241" MODIFIED="1607892065448" TEXT="1.3.3 MP results shall appear below search boxes">
+<node CREATED="1607892081115" ID="ID_1120607352" MODIFIED="1607892089400" TEXT="1.3.3.1 result cards">
+<node CREATED="1607892094044" ID="ID_1813784971" MODIFIED="1607892123368" TEXT="1.3.3.1.1 the left 7th of the box shall be block colour corresponding to party of mp"/>
+<node CREATED="1607892124235" ID="ID_1484114279" MODIFIED="1607892141496" TEXT="1.3.3.1.2 mp name shall appear to the right of the block colour"/>
+<node CREATED="1607892142412" ID="ID_314367039" MODIFIED="1607892166888" TEXT="1.3.3.1.3 &quot;MP for [constituency]&quot; shall appear to the right of mp name"/>
+<node CREATED="1607892167468" ID="ID_1754950922" MODIFIED="1607892208042" TEXT="1.3.3.1.4 when clicked, Selected MP History () shall be displayed">
+<icon BUILTIN="password"/>
+</node>
 </node>
 </node>
 </node>
