56a53875e5ee02f87b2b83845f97906269d83baa
Robert <1robertchambers@gmail.com>
2020-12-13 20:30:36 +0000

    add existing, update requirements for bills interface

--- functional_requirements.mm
+++ functional_requirements.mm
@@ -2,7 +2,7 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1607888125977" ID="ID_1111700057" MODIFIED="1607888576138" TEXT="Functional Requirements">
 <node CREATED="1607889805944" ID="ID_333862331" MODIFIED="1607889817745" POSITION="right" TEXT="1. client side">
-<node CREATED="1607889819446" ID="ID_1408814252" MODIFIED="1607890190462" TEXT="1.1 control panel">
+<node CREATED="1607889819446" ID="ID_1408814252" MODIFIED="1607890860048" TEXT="1.1 control panel">
 <node CREATED="1607889824804" ID="ID_1144532847" MODIFIED="1607889831174" TEXT="1.1.1 bills button">
 <node CREATED="1607889831178" ID="ID_1389117676" MODIFIED="1607889991228" TEXT="1.1.1.1 bills button shall occpuy left 1.3 of control panel"/>
 <node CREATED="1607889993381" ID="ID_1483981848" MODIFIED="1607890035938" TEXT="1.1.1.2 the graphic shall be a drawing of a document"/>
@@ -25,7 +25,39 @@
 </node>
 </node>
 </node>
-<node CREATED="1607890186982" ID="ID_1097690422" MODIFIED="1607890186982" TEXT=""/>
+<node CREATED="1607890186982" ID="ID_1097690422" MODIFIED="1607890496439" TEXT="1.2 bills interface">
+<node CREATED="1607890496443" ID="ID_1007733541" MODIFIED="1607890502839" TEXT="1.2.1 bill feed page">
+<node CREATED="1607890502842" ID="ID_1975689625" MODIFIED="1607890507799" TEXT="1.2.1.1 cards">
+<node CREATED="1607890507802" ID="ID_582132696" MODIFIED="1607890529539" TEXT="1.2.1.1.1 bill name at top of card"/>
+<node CREATED="1607890532582" ID="ID_33597705" MODIFIED="1607890550595" TEXT="1.2.1.1.2 short description shall appear below bill name"/>
+<node CREATED="1607890551078" ID="ID_791436401" MODIFIED="1607890567379" TEXT="1.2.1.1.3 favourite button shall appear below short description"/>
+<node CREATED="1607890895511" ID="ID_1542783817" MODIFIED="1607890906276" TEXT="1.2.1.1.3 when clicked, the corresponding bill details page will be displayed (see "/>
+</node>
+<node CREATED="1607890597639" ID="ID_1175379026" MODIFIED="1607890650435" TEXT="1.2.1.2 card organisation">
+<node CREATED="1607890670055" ID="ID_560372424" MODIFIED="1607890676707" TEXT="1.2.1.2.1 each section">
+<node CREATED="1607890677639" ID="ID_1044980659" MODIFIED="1607890694675" TEXT="1.2.1.2.1.1 title of section shall appear at the top of each section"/>
+<node CREATED="1607890695495" ID="ID_1225602991" MODIFIED="1607890997251" TEXT="1.2.1.2.1.2 small cards (see 1.2.1.1) shall appear below the title of the section, according to the title of the section (see 1.2.1.2.2)"/>
+<node CREATED="1607890928519" ID="ID_1075130625" MODIFIED="1607890949651" TEXT="1.2.1.2.1.3 swiping the section left/right shall progress to the previous/next card"/>
+</node>
+<node CREATED="1607890957160" ID="ID_621406590" MODIFIED="1607890962664" TEXT="1.2.1.2.2 sections">
+<node CREATED="1607890962667" ID="ID_204841961" MODIFIED="1607890973190" TEXT="1.2.1.2.2.1 new this week"/>
+<node CREATED="1607890973831" ID="ID_172854870" MODIFIED="1607891021236" TEXT="1.2.1.2.2.2 recently passed bills"/>
+</node>
+</node>
+</node>
+<node CREATED="1607891027736" ID="ID_1140242369" MODIFIED="1607891033668" TEXT="1.2.2 bill details page">
+<node CREATED="1607891034808" ID="ID_781604592" MODIFIED="1607891058356" TEXT="1.2.2.1 bill title shall appear at the top of the page"/>
+<node CREATED="1607891058840" ID="ID_923272176" MODIFIED="1607891075844" TEXT="1.2.2.2 longer bill description shall appear below the title"/>
+<node CREATED="1607891077288" ID="ID_1728513573" MODIFIED="1607891120964" TEXT="1.2.2.3 &quot;bill status&quot; text shall appear below longer bill description"/>
+<node CREATED="1607891121546" ID="ID_271776223" MODIFIED="1607891162500" TEXT="1.2.2.4 &quot;passed&quot;, &quot;blocked by lords&quot; etc shall appear below &quot;bill status&quot;, if the bill fate is known"/>
+<node CREATED="1607891163082" ID="ID_951571084" MODIFIED="1607891187621" TEXT="1.2.2.5 bill status graphic shall appear below bill status"/>
+<node CREATED="1607891220136" ID="ID_1362501188" MODIFIED="1607891403222" TEXT="1.2.2.6 aye/no boxes shall appear below bill status graphic, up to two of these may appear (2nd and 3rd reading votes)">
+<node CREATED="1607891239834" ID="ID_1961008119" MODIFIED="1607891287861" TEXT="1.2.2.6.1 two side by side boxes, left box titled &quot;noes&quot;, right box titled &quot;ayes&quot;"/>
+<node CREATED="1607891291672" ID="ID_1744950072" MODIFIED="1607891309061" TEXT="1.2.2.6.2 left box shall contain names of mps who voted no"/>
+<node CREATED="1607891309688" ID="ID_369266253" MODIFIED="1607891321734" TEXT="1.2.2.6.3 right box shall contain names of mps who voted aye"/>
+</node>
+</node>
+</node>
 </node>
 </node>
 </map>
