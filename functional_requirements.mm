f44b31a45bece82bf0e1dfd9ea609ed3ba535f47
Robert <1robertchambers@gmail.com>
2020-12-14 01:29:06 +0000

    add remaining existing, update requirements for Client Response Script

--- functional_requirements.mm
+++ functional_requirements.mm
@@ -108,7 +108,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1607895274741" ID="ID_289602725" MODIFIED="1607896700768" TEXT="1.4 Selected MP Interface">
+<node CREATED="1607895274741" ID="ID_289602725" MODIFIED="1607908269095" TEXT="1.4 Selected MP Interface">
 <node CREATED="1607895316309" ID="ID_1915670476" MODIFIED="1607900470273" TEXT="1.4.1 Selected MP History Page">
 <node CREATED="1607895338598" ID="ID_252788082" MODIFIED="1607895362178" TEXT="1.4.1.1 selected mp info box shall appear at the top of the page"/>
 <node CREATED="1607895366565" ID="ID_1930742941" MODIFIED="1607895386833" TEXT="1.4.1.2 search boxes shall appear below MP info box"/>
@@ -148,7 +148,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1607895582437" FOLDED="true" ID="ID_712218429" MODIFIED="1607896702232" TEXT="1.5 Preferences Page">
+<node CREATED="1607895582437" ID="ID_712218429" MODIFIED="1607908271100" TEXT="1.5 Preferences Page">
 <node CREATED="1607895689176" ID="ID_560983521" MODIFIED="1607895704802" TEXT="1.5.1 &quot;Preferences&quot; title shall appear at the top of the page"/>
 <node CREATED="1607895705286" ID="ID_431996279" MODIFIED="1607896396404" TEXT="1.5.2 Postcode Section shall appear below the &quot;Preferences&quot; title">
 <node CREATED="1607895720374" ID="ID_1723987520" MODIFIED="1607896105364" TEXT="1.5.2.1 title &quot;Postcode&quot; shall appear to the left"/>
@@ -158,10 +158,10 @@
 </node>
 <node CREATED="1607896369096" ID="ID_1395480176" MODIFIED="1607896388420" TEXT="1.5.3 Notifications Settings Section"/>
 </node>
-<node CREATED="1607896491272" ID="ID_104417048" MODIFIED="1607896503621" TEXT="1.6 Accounts Interface">
+<node CREATED="1607896491272" ID="ID_104417048" MODIFIED="1607908275849" TEXT="1.6 Accounts Interface">
 <node CREATED="1607896522169" ID="ID_1257197525" MODIFIED="1607896535061" TEXT="1.6.1 Login Interface"/>
 <node CREATED="1607896535352" ID="ID_1592389026" MODIFIED="1607896542661" TEXT="1.6.2 Sign Up Interface">
-<node CREATED="1607897020682" FOLDED="true" ID="ID_170183427" MODIFIED="1607899631062" TEXT="1.6.2.1 User Email Page">
+<node CREATED="1607897020682" ID="ID_170183427" MODIFIED="1607908218476" TEXT="1.6.2.1 User Email Page">
 <node CREATED="1607897245291" ID="ID_1899235563" MODIFIED="1607898090506" TEXT="1.6.2.1.1 textbox displayed centre of page">
 <node CREATED="1607897279757" ID="ID_1173248019" MODIFIED="1607897293064" TEXT="1.6.2.1.1.1 accept user text"/>
 <node CREATED="1607897293787" ID="ID_47034053" MODIFIED="1607897319303" TEXT="1.6.2.1.1.2 on pressing enter transfer email to server"/>
@@ -170,7 +170,7 @@
 <node CREATED="1607897331597" ID="ID_1005378580" MODIFIED="1607897457704" TEXT="1.6.2.1.2 If EMAILTAKEN error received from server display &quot;email taken&quot; text in red above email text box"/>
 <node CREATED="1607897653804" ID="ID_1782905491" MODIFIED="1607897682217" TEXT="1.6.2.1.3 if success received from server go to User Username Page"/>
 </node>
-<node CREATED="1607897038618" FOLDED="true" ID="ID_1729709490" MODIFIED="1607899631863" TEXT="1.6.2.2 User Username Page">
+<node CREATED="1607897038618" ID="ID_1729709490" MODIFIED="1607908219740" TEXT="1.6.2.2 User Username Page">
 <node CREATED="1607897732812" ID="ID_108365215" MODIFIED="1607898086474" TEXT="1.6.2.2.1 textbox displayed centre of page">
 <node CREATED="1607897754176" ID="ID_809614491" MODIFIED="1607897768345" TEXT="1.6.2.2.1.1 accept user text"/>
 <node CREATED="1607897768845" ID="ID_1606052373" MODIFIED="1607897789022" TEXT="1.6.2.2.1.2 on pressing enter transfer username to server"/>
@@ -179,7 +179,7 @@
 <node CREATED="1607898043262" ID="ID_1957549396" MODIFIED="1607898079034" TEXT="1.6.2.2.2 if USERNAMETAKEN error received from server display &quot;username taken&quot; text in red above email textbox"/>
 <node CREATED="1607898111278" ID="ID_1370382833" MODIFIED="1607898135098" TEXT="1.6.2.2.3 if success received from server go to User Password Page"/>
 </node>
-<node CREATED="1607897038618" FOLDED="true" ID="ID_118908833" MODIFIED="1607899632543" TEXT="1.6.2.3 User Constituency Page">
+<node CREATED="1607897038618" ID="ID_118908833" MODIFIED="1607908220621" TEXT="1.6.2.3 User Constituency Page">
 <node CREATED="1607897732812" ID="ID_1444603300" MODIFIED="1607898530587" STYLE="fork" TEXT="1.6.2.3.1 textbox displayed centre of page">
 <node CREATED="1607897754176" ID="ID_1734745213" MODIFIED="1607899516656" TEXT="1.6.2.3.1.1 accept user text"/>
 <node CREATED="1607897768845" ID="ID_1606227170" MODIFIED="1607898510434" TEXT="1.6.2.2.1.2 on pressing enter transfer postcode to server"/>
@@ -188,7 +188,7 @@
 <node CREATED="1607898043262" ID="ID_1663873864" MODIFIED="1607898079034" TEXT="1.6.2.2.2 if USERNAMETAKEN error received from server display &quot;username taken&quot; text in red above email textbox"/>
 <node CREATED="1607898111278" ID="ID_5411723" MODIFIED="1607898135098" TEXT="1.6.2.2.3 if success received from server go to User Password Page"/>
 </node>
-<node CREATED="1607897048458" FOLDED="true" ID="ID_262816662" MODIFIED="1607899633261" TEXT="1.6.2.3 User Password Page">
+<node CREATED="1607897048458" ID="ID_262816662" MODIFIED="1607908221420" TEXT="1.6.2.3 User Password Page">
 <node CREATED="1607897245291" ID="ID_689509135" MODIFIED="1607898172394" TEXT="1.6.2.3.1 textbox displayed centre of page">
 <node CREATED="1607897279757" ID="ID_1755946777" MODIFIED="1607898178858" TEXT="1.6.2.3.1.1 accept user text"/>
 <node CREATED="1607897293787" ID="ID_1401220753" MODIFIED="1607898283467" TEXT="1.6.2.3.1.2 on pressing enter transfer password to server"/>
@@ -261,18 +261,29 @@
 </node>
 <node CREATED="1607902197165" ID="ID_1145220626" MODIFIED="1607902212745" TEXT="2.2 Client response script">
 <node CREATED="1607903686016" ID="ID_1175044371" MODIFIED="1607903691691" TEXT="2.2.1 Accounts data">
-<node CREATED="1607904001985" ID="ID_515822062" MODIFIED="1607904318045" TEXT="2.2.1.1 Respond to email sent by client">
-<node CREATED="1607904319648" ID="ID_201437415" MODIFIED="1607904342253" TEXT="2.2.1.1.1 search database for email"/>
-<node CREATED="1607904346338" ID="ID_69679640" MODIFIED="1607906160130" TEXT="2.2.1.1.2 if email not found respond SUCCESS else respond EMAILTAKEN and listen again"/>
-<node CREATED="1607906225397" ID="ID_571059921" MODIFIED="1607906240706" TEXT="2.2.1.1.3 store email in database"/>
+<node CREATED="1607906861320" ID="ID_67788218" MODIFIED="1607906883188" TEXT="2.2.1.1 Sign Up data">
+<node CREATED="1607904001985" ID="ID_515822062" MODIFIED="1607906902436" TEXT="2.2.1.1.1 Respond to email sent by client">
+<node CREATED="1607904319648" ID="ID_201437415" MODIFIED="1607906915172" TEXT="2.2.1.1.1.1 search database for email"/>
+<node CREATED="1607904346338" ID="ID_69679640" MODIFIED="1607906924900" TEXT="2.2.1.1.1.2 if email not found respond SUCCESS else respond EMAILTAKEN and listen again"/>
+<node CREATED="1607906225397" ID="ID_571059921" MODIFIED="1607906943524" TEXT="2.2.1.1.1.3 store email in database"/>
 </node>
-<node CREATED="1607906019285" ID="ID_550084342" MODIFIED="1607906030834" TEXT="2.2.1.2 respond to username sent by client">
-<node CREATED="1607906032133" ID="ID_50405132" MODIFIED="1607906044177" TEXT="2.2.1.2.1 search databse for username"/>
-<node CREATED="1607906045846" ID="ID_416936223" MODIFIED="1607906155585" TEXT="2.2.1.2.2 if username not found respond SUCCESS else respond USERNAMETAKEN and listen again"/>
-<node CREATED="1607906184533" ID="ID_922577251" MODIFIED="1607906212033" TEXT="2.2.1.2.3 store username associated with email in database"/>
+<node CREATED="1607906019285" ID="ID_550084342" MODIFIED="1607906896292" TEXT="2.2.1.1.2 respond to username sent by client">
+<node CREATED="1607906032133" ID="ID_50405132" MODIFIED="1607906952453" TEXT="2.2.1.1.2.1 search databse for username"/>
+<node CREATED="1607906045846" ID="ID_416936223" MODIFIED="1607906964019" TEXT="2.2.1.1.2.2 if username not found respond SUCCESS else respond USERNAMETAKEN and listen again"/>
+<node CREATED="1607906184533" ID="ID_922577251" MODIFIED="1607907816136" TEXT="2.2.1.1.2.3 store username associated with email in database"/>
 </node>
-<node CREATED="1607906119365" ID="ID_1387242062" MODIFIED="1607906130993" TEXT="2.2.1.3 respond to password sent by client">
-<node CREATED="1607906132453" ID="ID_1486030079" MODIFIED="1607906657827" TEXT="2.2.1.3.1 store password and respond SUCCESS"/>
+<node CREATED="1607906119365" ID="ID_1387242062" MODIFIED="1607906892036" TEXT="2.2.1.1.3 respond to password sent by client">
+<node CREATED="1607906132453" ID="ID_1486030079" MODIFIED="1607907819416" TEXT="2.2.1.1.3.1 store password and respond SUCCESS"/>
+</node>
+</node>
+<node CREATED="1607907846413" ID="ID_1629426552" MODIFIED="1607908484986" TEXT="2.2.1.2 User Associated data">
+<node CREATED="1607907890188" ID="ID_1579633843" MODIFIED="1607907927416" TEXT="2.2.1.2.1 respond to favourite list request by client">
+<node CREATED="1607908301901" ID="ID_439886653" MODIFIED="1607908433769" TEXT="2.2.1.2.1.1 if request for users favourite list is valid, return favourite data, else return error"/>
+</node>
+<node CREATED="1607907940604" ID="ID_210069165" MODIFIED="1607907971353" TEXT="2.2.1.2.2 respond to constituency request sent by client">
+<node CREATED="1607908376303" ID="ID_1637102085" MODIFIED="1607908425146" TEXT="2.2.1.2.2.1 if request for users constituency is valid, return constituency as string, else return error"/>
+</node>
+<node CREATED="1607908496655" ID="ID_1802316478" MODIFIED="1607908510698" TEXT="2.2.1.2.3 respond to add favourite request by client"/>
 </node>
 </node>
 </node>
